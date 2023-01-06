56ea27e44f14f28063644ed660af74928781bed5
Jesús Lautaro Careglio Albornoz <j.l.careglio.albornoz@gmail.com>
2023-01-05 22:35:12 -0300

    :recycle: Formateado usando black y imports ordenados

--- price_ars.py
+++ price_ars.py
@@ -1,20 +1,22 @@
 import math
+
 from prices_list import prices_list
 
-def price_ars (app_id, games_to_scan, scanned_games):
 
-    price_list, volume_list, cards, success, game_name = prices_list(app_id, games_to_scan, scanned_games)
+def price_ars(app_id, games_to_scan, scanned_games):
+    price_list, volume_list, cards, success, game_name = prices_list(
+        app_id, games_to_scan, scanned_games
+    )
 
     price_cards = 0
-    cards_drop = math.ceil(cards/2)
+    cards_drop = math.ceil(cards / 2)
 
     if success == True:
 
         for price in price_list:
 
-            price_cards += float(price.replace('ARS$ ',''))
+            price_cards += float(price.replace("ARS$ ", ""))
 
-        price_cards = round(price_cards,2)
-
+        price_cards = round(price_cards, 2)
 
     return price_cards, cards_drop, cards, price_list, volume_list, success, game_name
--- price_game.py
+++ price_game.py
@@ -1,23 +1,29 @@
-from bs4 import BeautifulSoup
-import requests
 import json
 
+import requests
+from bs4 import BeautifulSoup
+
+
 def price_game(app_id, game_name):
-	url = "https://store.steampowered.com/api/appdetails?appids=" + str(app_id) + "&cc=ars&filters=price_overview"
-	datos = requests.get(url).content
-	soup = BeautifulSoup(datos,features="html.parser")
+    url = (
+        "https://store.steampowered.com/api/appdetails?appids="
+        + str(app_id)
+        + "&cc=ars&filters=price_overview"
+    )
+    datos = requests.get(url).content
+    soup = BeautifulSoup(datos, features="html.parser")
 
-	game_json = json.loads(str(soup))
+    game_json = json.loads(str(soup))
 
-	price_game = game_json[str(app_id)]["data"]["price_overview"]["final_formatted"]
-	game_discount = game_json[str(app_id)]["data"]["price_overview"]["discount_percent"]
+    price_game = game_json[str(app_id)]["data"]["price_overview"]["final_formatted"]
+    game_discount = game_json[str(app_id)]["data"]["price_overview"]["discount_percent"]
 
-	price_game = price_game.replace('ARS$ ','').replace('.','').replace(',','.')
+    price_game = price_game.replace("ARS$ ", "").replace(".", "").replace(",", ".")
 
-	"""if game_discount == 0:
+    """if game_discount == 0:
 		print("\nEl juego '{}' no se encuentra en descuento.\nEl valor del juego es {} ARS$".format(game_name, price_game))
 
 	else:
 		print("\nEl juego '{}' tiene un {}% de descuento.\nEl valor del juego en descuento es {} ARS$".format(game_name, game_discount, price_game))"""
 
-	return float(price_game)
+    return float(price_game)
