8ffc21602ec1ffb46d66c65c9a1885b420ef20ba
Jesús Lautaro Careglio Albornoz <j.l.careglio.albornoz@gmail.com>
2023-01-05 23:27:01 -0300

    :sparkles: Soporte para uso de proxy con proxiesapi

--- prices_list.py
+++ prices_list.py
@@ -1,14 +1,26 @@
-from bs4 import BeautifulSoup
-from get_hash import get_market_hash
-from colorama import init, Fore, Back, Style
-import requests, json, time, os
+import json
+import os
+import time
 
-def prices_list (app_id, games_to_scan, scanned_games):
+import requests
+from bs4 import BeautifulSoup
+from colorama import Back, Fore, Style, init
+from dotenv import load_dotenv
+
+from get_hash import get_market_hash
+
+
+def prices_list(app_id, games_to_scan, scanned_games):
 
     loaded_cards = 1
 
+    API_URL = os.getenv("API_ENDPOINT_URL")
+    AUTH_KEY = os.getenv("PROXIESAPI_AUTH_KEY")
+
     try:
-        market_hash, cards, game_name = get_market_hash(app_id, scanned_games, games_to_scan)
+        market_hash, cards, game_name = get_market_hash(
+            app_id, scanned_games, games_to_scan
+        )
 
     except:
         market_hash = []
@@ -19,24 +31,48 @@ def prices_list (app_id, games_to_scan, scanned_games):
     volume_list = []
     success = True
 
-    #market_hash = ['1140440-Pechenegs and Polovtsians', '1140440-OMON Soldier', '1140440-Riot Car', '1140440-Pay Respect to the President', '1140440-Bund of Karnavalny', '1140440-Agent "Cock"', '1140440-Erzhan', '1140440-Motorcade of the President', '1140440-Alien Invasion', '1140440-Soldier of FBK', '1140440-Ivan Temnoholmov', '1140440-Boris Haritonov', '1140440-Vladimir Vladimirovich', '1140440-"Are you a wolf?"', '1140440-Relaxing with friends', '1140440-Pechenegs and Polovtsians', '1140440-OMON Soldier', '1140440-Riot Car', '1140440-Pay Respect to the President', '1140440-Bund of Karnavalny', '1140440-Agent "Cock"', '1140440-Erzhan', '1140440-Motorcade of the President', '1140440-Alien Invasion', '1140440-Soldier of FBK', '1140440-Ivan Temnoholmov', '1140440-Boris Haritonov', '1140440-Vladimir Vladimirovich', '1140440-"Are you a wolf?"', '1140440-Relaxing with friends']
-    #market_hash = ['302490-Dark Elf']
-
     for card_hash in market_hash:
-        
-        url = "https://steamcommunity.com/market/priceoverview/?currency=34&appid=753&market_hash_name=" + card_hash.replace(' ','%20').replace("&", "%26").replace("/", "-")
-        datos = requests.get(url).content
-        soup = BeautifulSoup(datos,features="html.parser")
+        datos = None
+        url = (
+            "https://steamcommunity.com/market/priceoverview/?currency=34&appid=753&market_hash_name="
+            + card_hash.replace(" ", "%20")
+        )
+
+        if AUTH_KEY == None:
+            datos = requests.get(url).content
+        else:
+            PARAMS = {"auth_key": AUTH_KEY, "url": url}
+            datos = requests.get(url=API_URL, params=PARAMS).content
+
+        soup = BeautifulSoup(datos, features="html.parser")
         card_json = json.loads(str(soup))
 
-        points = '.' * (loaded_cards % 4)
+        points = "." * (loaded_cards % 4)
 
-        os.system('cls')
-        loading_text = Fore.CYAN + 'Cargando: {} cartas de {}{}'.format(loaded_cards, cards, points)
-        loading_bar = Fore.YELLOW + Style.DIM + '─   [ ' + Fore.WHITE + str('- ' * cards).replace('-',Fore.GREEN + '■' + Fore.WHITE, loaded_cards) + Fore.YELLOW + ']   ─' + Fore.WHITE
+        os.system("cls")
+        loading_text = Fore.CYAN + "Cargando: {} cartas de {}{}".format(
+            loaded_cards, cards, points
+        )
+        loading_bar = (
+            Fore.YELLOW
+            + Style.DIM
+            + "─   [ "
+            + Fore.WHITE
+            + str("- " * cards).replace(
+                "-", Fore.GREEN + "■" + Fore.WHITE, loaded_cards
+            )
+            + Fore.YELLOW
+            + "]   ─"
+            + Fore.WHITE
+        )
 
-       
-        print(Style.BRIGHT + 'App ID: ' + str(app_id) + '  ─  ' + 'Juego {} de {}\n'.format(scanned_games, games_to_scan))
+        print(
+            Style.BRIGHT
+            + "App ID: "
+            + str(app_id)
+            + "  ─  "
+            + "Juego {} de {}\n".format(scanned_games, games_to_scan)
+        )
         print(loading_text)
         print(loading_bar)
 
@@ -44,65 +80,59 @@ def prices_list (app_id, games_to_scan, scanned_games):
         time.sleep(2.5)
 
         try:
-            success = card_json['success']
+            success = card_json["success"]
         except:
-            print('Problema con el cromo')
+            print("Problema con el cromo")
             pass
 
         if success == True:
 
             try:
-                price_card = card_json['lowest_price']
+                price_card = card_json["lowest_price"]
             except:
-                price_card = '0'
-                print('Problema con el lowest_price')
+                price_card = "0"
+                print("Problema con el lowest_price")
 
             try:
-                volume = card_json['volume']
+                volume = card_json["volume"]
             except:
-                volume = '0'
-                #print('Problema con el Volumen')
+                volume = "0"
+                # print('Problema con el Volumen')
 
-            prices += price_card.replace('.','').replace(',','.'),
-            volume_list += volume,
-
+            prices += (price_card.replace(".", "").replace(",", "."),)
+            volume_list += (volume,)
 
         else:
             try:
-                url += '%20%28Trading%20Card%29'
+                url += "%20%28Trading%20Card%29"
                 datos = requests.get(url).content
-                soup = BeautifulSoup(datos,features="html.parser")
+                soup = BeautifulSoup(datos, features="html.parser")
                 card_json = json.loads(str(soup))
 
-                price_card = card_json['lowest_price']
-                success = card_json['success']
+                price_card = card_json["lowest_price"]
+                success = card_json["success"]
 
                 try:
-                    volume = card_json['volume']
+                    volume = card_json["volume"]
                 except:
-                    volume = '0'
+                    volume = "0"
 
-                prices += price_card.replace(',','.'),
-                volume_list += volume,
-
+                prices += (price_card.replace(",", "."),)
+                volume_list += (volume,)
 
             except:
-                volume = '0'
-                price_card = '0'
+                volume = "0"
+                price_card = "0"
                 success = False
 
                 break
 
     if len(market_hash) == 0:
-            volume = '0'
-            price_card = '0'
-            prices = []
-            volume_list = []
-            cards = 0
-            success = False
-
-    
+        volume = "0"
+        price_card = "0"
+        prices = []
+        volume_list = []
+        cards = 0
+        success = False
 
     return prices, volume_list, cards, success, game_name
-
-#print(prices_list(280140, 0, 0))
